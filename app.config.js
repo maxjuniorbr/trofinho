13529c0c2d869e724dbd1dbda8809d9e5326da63
maxjuniorbr <maxjuniorbr@gmail.com>
2026-04-07 12:05:18 -0300

    chore: update audit fixes and gradle jvm args

--- app.config.js
+++ app.config.js
@@ -1,4 +1,19 @@
+const { withGradleProperties } = require('@expo/config-plugins');
+
 module.exports = function appConfig({ config }) {
+  config = withGradleProperties(config, (c) => {
+    const existing = c.modResults.findIndex(
+      (item) => item.type === 'property' && item.key === 'org.gradle.jvmargs'
+    );
+    const value = '-Xmx4g -XX:MaxMetaspaceSize=1g -XX:+HeapDumpOnOutOfMemoryError';
+    if (existing >= 0) {
+      c.modResults[existing].value = value;
+    } else {
+      c.modResults.push({ type: 'property', key: 'org.gradle.jvmargs', value });
+    }
+    return c;
+  });
+
   return {
     ...config,
     extra: {
