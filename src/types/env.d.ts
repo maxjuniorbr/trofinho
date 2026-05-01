fdf27c8d04e151d81e3fa1e9040d9e7395ae42a2
Paulo César Dianin Junior <maxjuniorbr@gmail.com>
2026-05-01 05:23:13 -0300

    chore: add sentry config and secure email template urls

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -4,6 +4,7 @@ declare global {
       EXPO_PUBLIC_SUPABASE_URL: string;
       EXPO_PUBLIC_SUPABASE_ANON_KEY: string;
       EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID: string;
+      EXPO_PUBLIC_SENTRY_DSN?: string;
       GOOGLE_ANDROID_CLIENT_ID: string;
       EXPO_OS: 'android';
     }
